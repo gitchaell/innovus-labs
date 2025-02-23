3aff3c71201dfacf00c35c4d2e8aead853e7dde8
gitchaell <f.michaell.a.m@gmail.com>
2025-02-23 18:05:33 -0400

    feat: capacitor camera

--- exercise-2/src/app/page.tsx
+++ exercise-2/src/app/page.tsx
@@ -1,96 +1,76 @@
-import Image from 'next/image'
+'use client'
+import { useState } from 'react'
+import { Camera, CameraResultType } from '@capacitor/camera'
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Snackbar from '@mui/material/Snackbar'
+import Typography from '@mui/material/Typography'
+import Card from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia'
 
 export default function Home() {
-	return (
-		<div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
-			<main className='row-start-2 flex flex-col items-center gap-8 sm:items-start'>
-				<Image
-					className='dark:invert'
-					src='/next.svg'
-					alt='Next.js logo'
-					width={180}
-					height={38}
-					priority
-				/>
-				<ol className='list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left'>
-					<li className='mb-2'>
-						Get started by editing{' '}
-						<code className='rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]'>
-							src/app/page.tsx
-						</code>
-						.
-					</li>
-					<li>Save and see your changes instantly.</li>
-				</ol>
+	const [photo, setPhoto] = useState<string | null>(null)
+	const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
-				<div className='flex flex-col items-center gap-4 sm:flex-row'>
-					<a
-						className='flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-sm text-background transition-colors hover:bg-[#383838] sm:h-12 sm:px-5 sm:text-base dark:hover:bg-[#ccc]'
-						href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-						target='_blank'
-						rel='noopener noreferrer'>
-						<Image
-							className='dark:invert'
-							src='/vercel.svg'
-							alt='Vercel logomark'
-							width={20}
-							height={20}
+	const handleTakePhoto = async (): Promise<void> => {
+		setErrorMessage(null)
+
+		Camera.getPhoto({
+			resultType: CameraResultType.DataUrl,
+			allowEditing: true,
+			correctOrientation: true,
+			webUseInput: false,
+		})
+			.then((image) => {
+				setPhoto(image.dataUrl || null)
+			})
+			.catch((error) => {
+				setErrorMessage('Camera not supported in this environment')
+				console.error('Camera not supported in this environment', error)
+			})
+	}
+
+	const handleCloseMessage = () => {
+		setErrorMessage(null)
+	}
+
+	return (
+		<Container maxWidth='lg'>
+			<Box
+				sx={{
+					my: 4,
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}>
+				<Typography variant='h5'>Capacitor Camera</Typography>
+
+				<Button
+					variant='contained'
+					onClick={handleTakePhoto}>
+					Tomar Foto
+				</Button>
+
+				{photo && (
+					<Card>
+						<CardMedia
+							component='img'
+							image={photo}
+							alt='Foto'
 						/>
-						Deploy now
-					</a>
-					<a
-						className='flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]'
-						href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-						target='_blank'
-						rel='noopener noreferrer'>
-						Read our docs
-					</a>
-				</div>
-			</main>
-			<footer className='row-start-3 flex flex-wrap items-center justify-center gap-6'>
-				<a
-					className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-					href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'>
-					<Image
-						aria-hidden
-						src='/file.svg'
-						alt='File icon'
-						width={16}
-						height={16}
-					/>
-					Learn
-				</a>
-				<a
-					className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-					href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'>
-					<Image
-						aria-hidden
-						src='/window.svg'
-						alt='Window icon'
-						width={16}
-						height={16}
-					/>
-					Examples
-				</a>
-				<a
-					className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-					href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'>
-					<Image
-						aria-hidden
-						src='/globe.svg'
-						alt='Globe icon'
-						width={16}
-						height={16}
-					/>
-					Go to nextjs.org →
-				</a>
-			</footer>
-		</div>
+					</Card>
+				)}
+
+				<Snackbar
+					anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+					open={errorMessage !== null}
+					onClose={handleCloseMessage}
+					message={errorMessage || ''}
+					key='top-center'
+				/>
+			</Box>
+		</Container>
 	)
 }
